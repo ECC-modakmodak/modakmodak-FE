3474445e132b99de9067a08e4266d97b89abafe4
수안 <rla.swan@ewhain.net>
2026-01-28 13:59:02 +0900

    feat: 공통 Input 컴포넌트 구현

---
+++ src/components/common/Input.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import styled from '@emotion/styled';
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: ${(props) => props.width || '100%'};
+  margin-bottom: 16px;
+`;
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  height: 34px;
+  font-family: 'Pretendard';
+  font-size: 16px;
+  color: #000;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-bottom: 4px;
+`;
+
+const StyledInput = styled.input`
+  width: ${(props) => props.width || '100%'};
+  height: 61px;
+  padding: 0 16px;
+  border: 1px solid #D9695C;
+  border-radius: 10px;
+  outline: none;
+  font-size: 16px;
+  background-color: #FFF;
+
+  &::placeholder {
+    color: #A5A5A5;
+  }
+
+  &:focus {
+    border-color: #D9695C;
+  }
+`;
+
+const HelperText = styled.div`
+  font-size: 16px;
+  margin-top: 4px;
+  color: #D9695C;
+`;
+
+const Input = ({ label, helperText, error, width, ...props }) => {
+  return (
+    <InputWrapper width={width}>
+      {label && <Label>{label}</Label>}
+      <StyledInput error={error} {...props} />
+      {helperText && <HelperText error={error}>{helperText}</HelperText>}
+    </InputWrapper>
+  );
+};
+
+export default Input;
