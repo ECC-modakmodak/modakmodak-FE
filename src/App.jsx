f4dfbaf8cbe3fc1238e87121b9e7cb831441a41a
yeseul <joyri0202@gmail.com>
2026-01-30 00:37:51 +0900

    chore: 컴포넌트 확인용 테스트 파일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,34 @@
-import './App.css';
-import {
-  CheerUp,
-  GoodJob,
-  Hi,
-  NeedHelp,
-  NiceToMeet,
-  OnMyWay,
-  RunningLate,
-  Tired,
-  WorkingHard,
-} from './components/common/tagChip/Badge';
-import Goal from './components/common/tagChip/Goal';
 import { useState } from 'react';
-import Remind from './components/common/tagChip/Remind';
-import { Chatty, Quiet } from './components/common/tagChip/Mood';
-import { Cafe, Cam, Other, Zoom } from './components/common/tagChip/StudyType';
-import Today from './components/common/tagChip/Today';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [value, setValue] = useState('');
-  const [completed, setCompleted] = useState(false);
+  const [count, setCount] = useState(0);
 
   return (
-    <div
-      className="App"
-      style={{ backgroundColor: '#ffffff', padding: '50px' }}
-    >
-      {/* 목표 입력 테스트 */}
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          marginBottom: '30px',
-        }}
-      >
-        <Goal
-          value={value}
-          completed={completed}
-          onChange={(e) => setValue(e.target.value)}
-          onComplete={() => setCompleted(true)}
-        />
-      </div>
-      {/* 상태 배지 테스트 */}
-      <div
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          gap: '10px',
-        }}
-      >
-        <Hi />
-        <NiceToMeet />
-        <CheerUp />
-        <WorkingHard />
-        <OnMyWay />
-        <Tired />
-        <NeedHelp />
-        <RunningLate />
-        <GoodJob />
-      </div>
-      {/* 회고 테스트 */}
-      <div style={{ marginTop: '30px' }}>
-        <Remind />
-      </div>
-      {/* 공부 태그 테스트 */}
+    <>
       <div>
-        {/* 분위기 태그 */}
-        <div
-          style={{
-            marginTop: '30px',
-            display: 'flex',
-            justifyContent: 'center',
-            gap: '10px',
-          }}
-        >
-          <Chatty />
-          <Quiet />
-        </div>
-        {/* 공부 유형 태그 */}
-        <div
-          style={{
-            marginTop: '30px',
-            display: 'flex',
-            justifyContent: 'center',
-            gap: '10px',
-          }}
-        >
-          <Cafe />
-          <Zoom />
-          <Cam />
-          <Other />
-        </div>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      {/* 오늘의 팟  */}
-      <div
-        style={{
-          marginTop: '30px',
-          display: 'flex',
-          justifyContent: 'center',
-          gap: '10px',
-        }}
-      >
-        <Today label="이화여대" />
-        <Today label="21:00" />
-        <Today label="모각코" />
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
-    </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
 }
 
