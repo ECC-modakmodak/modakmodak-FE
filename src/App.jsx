28054be81f4c98bb941e7188e89ce6caac3bc48c
수안 <rla.swan@ewhain.net>
2026-02-07 11:10:55 +0900

    Chore: App.jsx 라우팅 테스트를 위한 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import './index.css';
 import ComponentTest from './pages/component-test';
+import LoginPage from './pages/Login';
+import OnboardingPage from './pages/Onboarding';
+import Signup from './pages/Signup';
 
 /* 임시 홈 */
 function Home() {
@@ -12,6 +15,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           {/* 다른 라우트들은 여기에 추가 */}
